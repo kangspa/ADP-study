992ffa3ceb04a4d26f1f1e422a3e043084b055d9
kangspa <111270327+kangspa@users.noreply.github.com>
2025-09-30 22:30:08 +0900

    add:RData-시계열데이터추가

--- PracticeTest/RData/export_R_data.R
+++ PracticeTest/RData/export_R_data.R
@@ -9,12 +9,21 @@ data(USArrests)
 data(pressure)
 data(Titanic)
 
-# ggplot2 패키지의 diamonds 데이터
+# ggplot2 패키지의 diamonds, economics 데이터
 library(ggplot2)
 data(diamonds)
+data(economics)
+
+# EuStockMarkets, nottem (시계열 데이터)
+data(EuStockMarkets)
+data(nottem)
 
 # Titanic은 table 형식이므로 데이터프레임으로 변환
 Titanic_df <- as.data.frame(Titanic)
+
+# 시계열 객체는 data.frame으로 변환
+EuStockMarkets_df <- as.data.frame(EuStockMarkets)
+nottem_df <- data.frame(date = time(nottem), temp = as.numeric(nottem))
 
 # 내보낼 데이터 리스트
 datasets <- list(
@@ -27,7 +36,10 @@ datasets <- list(
   ChickWeight = ChickWeight,
   USArrests = USArrests,
   pressure = pressure,
-  Titanic = Titanic_df
+  Titanic = Titanic_df,
+  economics = economics,
+  EuStockMarkets = EuStockMarkets_df,
+  nottem = nottem_df
 )
 
 # CSV로 내보내기 (현재 작업 디렉토리에 저장됨)
