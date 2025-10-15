5ea50ab3cd4096ac8a527416b4a432d27f2958bc
kangspa <111270327+kangspa@users.noreply.github.com>
2025-10-15 17:23:31 +0900

    edit:PracticeTest/OpenBook-PDF/merge_pdf.py

--- PracticeTest/OpenBook-PDF/merge_pdf.py
+++ PracticeTest/OpenBook-PDF/merge_pdf.py
@@ -1,6 +1,7 @@
 import os
 from PyPDF2 import PdfMerger, PdfReader
 
+# 하위 디렉터리까지 포함해서 파일 검색하기 (ext로 확장자 지정 가능)
 def find_files(ext=None, base_dir='.'):
     result = []
     for root, dirs, files in os.walk(base_dir):
@@ -10,16 +11,19 @@ def find_files(ext=None, base_dir='.'):
                     result.append(os.path.join(root, file))
             else: result.append(os.path.join(root, file))
     return result
-
+# pdf 파일 검색 후 파일 갯수 출력(파일명은 주석처리)
 pdf_files = find_files('pdf')
 print(f"총 {len(pdf_files)}개의 파일 발견:")
 # for pdf in pdf_files: print(f"  {pdf}")
 
+# pdf 파일 병합
 merger = PdfMerger()
 for pdf in pdf_files:
     merger.append(PdfReader(open(pdf, 'rb')))
-    print(f"  {pdf}")
+    print(f"  {pdf}") # 병합 파일 출력
 
-merger.write("제35회ADP실기오픈북[251015].pdf")
+# 병합된 pdf 파일 저장 / 저장 완료 출력
+fileName = "제35회ADP실기오픈북[251015].pdf"
+merger.write(fileName)
 merger.close()
-print("제35회ADP실기오픈북[251015].pdf 생성 완료")
+print(fileName, "생성 완료")
